473eece577de15558daf05e7bdddb118a43ceb18
Fahad337681 <fhd10839@gmail.com>
2026-04-08 11:57:54 +0500

    Information security Lab Python practice

---
+++ dictionary.py
@@ -0,0 +1,19 @@
+Dict={
+    1:'Fahad',
+    2:'Masaud',
+    3:'Faheem'
+}
+
+print(Dict)
+
+Dict_1={}
+Dict_1[0]={3}
+Dict_1[1]={6}
+Dict_1[2]={9}
+Dict_1[3]={12}
+
+print(Dict_1)
+
+del Dict[2]
+print("Dictionary after removing value at key 2")
+print(Dict)
